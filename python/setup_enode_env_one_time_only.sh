cfdaaffb58d3a3267f7505166d1cb7c131533855
João L. Vieira <105939582+jvieira9@users.noreply.github.com>
2025-07-11 15:47:42 +0000

    Update setup_enode_env_one_time_only.sh

--- python/setup_enode_env_one_time_only.sh
+++ python/setup_enode_env_one_time_only.sh
@@ -2,31 +2,14 @@
 
 set -e
 
-echo "Setting up Enode Python environment..."
-
 apt-get update
-apt-get install -y python3-venv
+apt-get install -y python3-pip
 
-python3 -m venv venv
-
-./venv/bin/pip install --upgrade pip
-./venv/bin/pip install flask gunicorn python-dotenv mysql-connector-python
-
-ENV_FILE=".env"
-if [[ ! -f $ENV_FILE ]]; then
-    echo "Creating .env file..."
-    cat <<EOF > $ENV_FILE
-DB_HOST=database-mysql.ctl4ffo64vfd.us-east-1.rds.amazonaws.com
-DB_USER=admin
-DB_PASS=Passw0rd
-DB_NAME=enode
-EOF
-else
-    echo ".env file already exists, skipping creation."
-fi
+pip3 install --upgrade pip
+pip3 install flask gunicorn python-dotenv mysql-connector-python
 
 if [[ -f requirements.txt ]]; then
-    ./venv/bin/pip install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     echo "requirements.txt not found, skipping installation."
 fi
